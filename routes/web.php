3cb9ef6258f140d4ac9842527b41b7a20e70b13b
Prashant Tondale <86417394+prashantpgit@users.noreply.github.com>
2023-01-27 20:19:58 +0530

    Update web.php
    Remove comments

--- routes/web.php
+++ routes/web.php
@@ -30,55 +30,9 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-//CRUD operations
 
-/* //List all notes
-Route::get('/notes', );
-
-
-//List single note
-Route::get('/notes/{{note}}', );
-
-
-//Route to display create note form
-Route::get('/notes/create', );
-
-//Post route to save created note
-
-Route::post('notes', );
-
-//edit
-
-
-//update
-
-
-//destroy */
-
-//This line alone will generate all required resource routes using controller
-/* Route::resource('notes',{{ CONTROLLER }} ); */
-//with middleware(['auth']) laravel makes sure that none of this is available without login
 Route::resource('notes', NoteController::class)->
 middleware(['auth']);
-
-/* Route::get('/trashed', [TrashedNoteController::class, 'index'])
-->middleware('auth')
-->name('trashed.index');
-
-Route::get('/trashed/{note}', [TrashedNoteController::class, 'show'])
-->withTrashed()
-->middleware('auth')
-->name('trashed.show');
-
-Route::put('/trashed/{note}', [TrashedNoteController::class, 'update'])
-->withTrashed()
-->middleware('auth')
-->name('trashed.update');
-
-Route::delete('/trashed/{note}', [TrashedNoteController::class, 'destroy'])
-->withTrashed()
-->middleware('auth')
-->name('trashed.destroy'); */
 
 Route::prefix('/trashed')->name('trashed.')->middleware('auth')->group(function(){
     Route::get('/', [TrashedNoteController::class, 'index'])->name('index');
