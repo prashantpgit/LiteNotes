c01cb802b728ffeb5c83258930e0e9c526ae6a5d
Prashant Tondale <86417394+prashantpgit@users.noreply.github.com>
2023-01-27 20:22:51 +0530

    Update WelcomeController.php

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -9,23 +9,6 @@ use Illuminate\Support\Facades\DB;
 class WelcomeController extends Controller
 {
     public function index(){
-        //1. Using raw SQL queries
-/*         $users = DB::select('select * from users');
-        dd($users); */
-
-        //2 . Using query builder
-     /*    $users = DB::table('users')->select(['name', 'email'])->whereNotNull('email')->orderBy('name')->get();
-        dd($users); */
-
-        //3. Using Eloquent ORM
-
-       /* $students =  Student::select(['name', 'email'])->whereNotNull('email')->orderBy('name')->get();
-       dd($students); */
-
-    /*    foreach($students as $student){
-         echo $student->name."<br/>";
-       } */
-
        $student = new Student();
        $student->name = "rahul";
        $student->email = "rahul@admin.com";
